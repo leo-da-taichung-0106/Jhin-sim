2ca4167b7d8e1b71b37cc2e56d8d249d3e2581d5
leo-da-taichung-0106 <46314406+leo-da-taichung-0106@users.noreply.github.com>
2022-02-16 16:36:03 -0800

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,23 @@
+--x =20
+x= io.read()
+	y=x-1
+x=1
+	print("In carnage, I bloom. Like a flower in the dawn")
+for i=0,y 
+do 
+if x==4
+then
+	print("4!!")
+	
+elseif x%4==0
+then
+	local a = x/4;
+	print("4 * ".. a.."!!")
+	
+else
+	
+	print(x) ;
+end
+x=x+1 	
+end
+print("The Show Never Ends! ")
